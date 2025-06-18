4591af2c4be0bdefea26d0016c9748a1f0cc115f
Daniel Maramba <daniel413x@gmail.com>
2025-06-17 23:37:43 -0400

    force dynamic on auth pages

--- src/app/(app)/(auth)/sign-in/page.tsx
+++ src/app/(app)/(auth)/sign-in/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import SignInView from "@/modules/auth/ui/views/SignInView";
 import { caller } from "@/trpc/server";
 import { redirect } from "next/navigation";
