4eb025b028cde45f67260ca07c40257bbfde6fc8
Daniel Maramba <daniel413x@gmail.com>
2025-06-30 15:09:53 -0400

    fix prefetching implementation

--- src/app/(app)/(root)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(root)/[category]/[subcategory]/page.tsx
@@ -3,6 +3,7 @@ import { getQueryClient, trpc } from "@/trpc/server";
 import ProductListView from "@/modules/products/ui/views/ProductListView";
 import { loadProductFilters } from "@/modules/products/searchParams";
 import { SearchParams } from "nuqs/server";
+import { DEFAULT_LIMIT } from "@/lib/data/constants";
 
 interface SubcategoryPageProps {
   params: Promise<{
@@ -19,10 +20,13 @@ const SubcategoryPage = async ({
   const filters = await loadProductFilters(searchParams);
   const queryClient = getQueryClient();
   // fetch products data server-side
-  void queryClient.prefetchQuery(
-    trpc.products.getMany.queryOptions({
-      category: subcategory,
+  // in child components, access prefetched data with useSuspenseInfiniteQuery
+  // NOT useSuspenseQuery!
+  void queryClient.prefetchInfiniteQuery(
+    trpc.products.getMany.infiniteQueryOptions({
       ...filters,
+      category: subcategory,
+      limit: DEFAULT_LIMIT,
     })
   );
   return (
