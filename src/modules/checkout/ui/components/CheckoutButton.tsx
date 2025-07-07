1a12da77b4ccc1454af37818de921979866e9eaa
Daniel Maramba <daniel413x@gmail.com>
2025-07-07 03:03:49 -0400

    add cart state

---
+++ src/modules/checkout/ui/components/CheckoutButton.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/common/shadcn/button";
+import { useCart } from "../../hooks/useCart";
+import { cn, generateTenantURL } from "@/lib/utils";
+import Link from "next/link";
+import { CHECKOUT_ROUTE } from "@/lib/data/routes";
+import { ShoppingCartIcon } from "lucide-react";
+
+interface CheckoutButtonProps {
+  className?: string;
+  hideIfEmpty?: boolean;
+  tenantSlug: string;
+}
+
+const CheckoutButton = ({
+  className = "",
+  hideIfEmpty = false,
+  tenantSlug,
+}: CheckoutButtonProps) => {
+  const { totalItems } = useCart(tenantSlug);
+  if (hideIfEmpty && totalItems === 0) {
+    return null;
+  }
+  let ariaLabel = "";
+  if (totalItems === 1) {
+    ariaLabel = "Your cart. There is 1 item added.";
+  } else if (totalItems > 1) {
+    ariaLabel = `Your cart. There are ${totalItems} items added.`;
+  } else {
+    ariaLabel = "Your cart. There are no items added.";
+  }
+  return (
+    <Button variant="elevated" asChild className={cn("bg-white", className)} aria-label={ariaLabel}>
+      <Link href={`/${generateTenantURL(tenantSlug)}/${CHECKOUT_ROUTE}`}>
+        <ShoppingCartIcon />
+        {totalItems > 0 ? totalItems : ""}
+      </Link>
+    </Button>
+  );
+};
+
+export default CheckoutButton;
