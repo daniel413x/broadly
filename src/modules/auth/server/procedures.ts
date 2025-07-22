029b0cb8ce7636a54f26305b27cffba05af26c45
Daniel Maramba <daniel413x@gmail.com>
2025-07-22 15:25:33 -0400

    integrate stripe connect

--- src/modules/auth/server/procedures.ts
+++ src/modules/auth/server/procedures.ts
@@ -3,6 +3,7 @@ import { TRPCError } from "@trpc/server";
 import { headers as getHeaders } from "next/headers";
 import { loginSchema, registerSchema } from "../schemas";
 import { generateAuthCookie } from "../utils";
+import { stripe } from "@/lib/stripe";
 
 export const authRouter = createTRPCRouter({
   // ctx being passed down is not native to tRPC
@@ -33,12 +34,19 @@ export const authRouter = createTRPCRouter({
           message: "Username already taken",
         });
       }
+      const account = await stripe.accounts.create({});
+      if (!account) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to create Stripe account",
+        });
+      }
       const tenant = await ctx.db.create({
         collection: "tenants",
         data: {
           name: input.username,
           slug: input.username,
-          stripeAccountId: "test",
+          stripeAccountId: account.id,
         },
       });
       await ctx.db.create({
--- src/modules/tenants/ui/components/Navbar.tsx
+++ src/modules/tenants/ui/components/Navbar.tsx
@@ -37,20 +37,20 @@ const Navbar = ({
   return (
     <nav className="h-20 border-b font-medium bg-white">
       <div className="max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full px-4 lg:px-12">
-        <Link href={`/${generateTenantURL(slug)}`} className="flex items-center gap-2">
-          {!tenant.image?.url ? null : (
+        <div className="flex gap-2">
+          <Link href={`/${generateTenantURL(slug)}`} className="flex items-center gap-2">
             <Image
-              src={tenant.image?.url || "/images/tenant-placeholder.png"}
+              src={tenant.image?.url || "/author-avatar-placeholder.png"}
               alt={slug}
               width={32}
               height={32}
               className="rounded-full border shrink-0 size-[32px]"
             />
-          )}
-        </Link>
-        <p className="text-xl">
-          {tenant.name}
-        </p>
+          </Link>
+          <p className="text-xl">
+            {tenant.name}
+          </p>
+        </div>
         <CheckoutButton
           tenantSlug={slug}
         />
